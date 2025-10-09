2ff0ee83614b5a4014f8704e78bd2c157af88e9a
Deep <ghandizadehmohammadamir@gmail.com>
2025-10-09 23:06:24 +0330

    added blog app and I sorted out the templates in base.html
    + blog app added with urls and views
    + blog app added in settings.py
    + base.html added in templates
    + header and footer added in base.html
    + template codes sorted in blocks and extends
    + fixed img and css sources with static in templates
    + fixed url paths in templates

--- 02_Django_Exercises/My_site/website/urls.py
+++ 02_Django_Exercises/My_site/website/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from website.views import *
 
+app_name="website"
+
 urlpatterns = [
-    path("", home_view),
-    path("support", support_view),
-    path("about", about_view)
+    path("", home_view, name="index"),
+    path("contact", contact_view, name="contact"),
+    path("about", about_view, name="about"),
+    path("test", test_view, name="test"),
+    
 ]
